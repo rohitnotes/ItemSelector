0d2181f41c913f0d61e2ee885f4e1e2f02dccf67
Tony Yang <superzoro168@gmail.com>
2019-07-31 23:11:46 +0800

    Scroll to last item when add new item

--- app/src/main/java/com/tonyyang/common/itemselector/member/selector/MemberSelectorFragment.kt
+++ app/src/main/java/com/tonyyang/common/itemselector/member/selector/MemberSelectorFragment.kt
@@ -1,27 +1,29 @@
 package com.tonyyang.common.itemselector.member.selector
 
 import android.app.Activity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.tonyyang.common.itemselector.database.Member
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tonyyang.common.itemselector.R
+import com.tonyyang.common.itemselector.SeparatorDecoration
+import com.tonyyang.common.itemselector.database.Member
 import com.tonyyang.common.itemselector.member.NewMemberActivity
+import com.tonyyang.common.itemselector.parcel
 import com.tonyyang.common.itemselector.util.TestUtils
 import kotlinx.android.synthetic.main.fragment_select_member.*
-import com.tonyyang.common.itemselector.SeparatorDecoration
 
 
-class MemberSelectorFragment: Fragment() {
+class MemberSelectorFragment : Fragment() {
 
     companion object {
         private const val NEW_WORD_ACTIVITY_REQUEST_CODE = 1
@@ -37,6 +39,8 @@ class MemberSelectorFragment: Fragment() {
     private val mMemberViewModel by lazy {
         ViewModelProviders.of(this).get(MemberSelectorViewModel::class.java)
     }
+
+    private var isAddNewMember = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_select_member, container, false)
@@ -57,16 +61,25 @@ class MemberSelectorFragment: Fragment() {
         recyclerView.apply {
             setHasFixedSize(true)
             adapter = mAdapter
-            layoutManager = androidx.recyclerview.widget.LinearLayoutManager(activity)
-            addItemDecoration(SeparatorDecoration.Builder(context)
-                .setMargin(16F, 0F)
-                .setHeaderCount(1)
-                .build())
+            layoutManager = LinearLayoutManager(activity)
+            addItemDecoration(
+                SeparatorDecoration.Builder(context)
+                    .setMargin(16F, 0F)
+                    .setHeaderCount(1)
+                    .build()
+            )
         }
 
         mMemberViewModel.getAllMembers().observe(this, Observer<List<Member>> {
             // Update the cached copy of the members in the adapter
             mAdapter.update(it)
+            if (isAddNewMember) {
+                recyclerView.smoothScrollToPosition(
+                    mMemberViewModel.getAllMembers().value?.size
+                        ?.plus(mAdapter.mHeaderCnt)?.minus(1).parcel()
+                )
+                isAddNewMember = false
+            }
         })
 
         mMemberViewModel.getIsUpdating().observe(this, Observer<Boolean> { isVisible ->
@@ -79,7 +92,8 @@ class MemberSelectorFragment: Fragment() {
 
         fab.setOnClickListener {
             val intent = Intent(activity, NewMemberActivity::class.java)
-            startActivityForResult(intent,
+            startActivityForResult(
+                intent,
                 NEW_WORD_ACTIVITY_REQUEST_CODE
             )
         }
@@ -100,6 +114,7 @@ class MemberSelectorFragment: Fragment() {
                 photoPath
             )
             mMemberViewModel.addNewMember(member)
+            isAddNewMember = true
         } else {
             Toast.makeText(activity, R.string.empty_not_saved, Toast.LENGTH_LONG).show()
         }
