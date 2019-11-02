0e986342955782e80c5aa3139cd25d217289b877
Tony Yang <superzoro168@gmail.com>
2019-11-02 08:38:11 +0800

    Don't show message when member not created

--- app/src/main/java/com/tonyyang/common/itemselector/member/selector/MemberSelectorFragment.kt
+++ app/src/main/java/com/tonyyang/common/itemselector/member/selector/MemberSelectorFragment.kt
@@ -9,7 +9,6 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -115,8 +114,6 @@ class MemberSelectorFragment : Fragment() {
             )
             mMemberViewModel.addNewMember(member)
             isAddNewMember = true
-        } else {
-            Toast.makeText(activity, R.string.empty_not_saved, Toast.LENGTH_LONG).show()
         }
     }
 }
